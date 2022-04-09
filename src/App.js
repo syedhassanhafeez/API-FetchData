f6261dfc146e9c64bb0db5cbd15914f13efe167d
unknown <Syed Hassan>
2022-04-09 14:32:24 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react"
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+  const [post, setPost] = useState([])
+  useEffect(() => {
+    const url = 'https://jsonmock.hackerrank.com/api/movies/search/?Title=spiderman&page=1'
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        return await response.json();
+        // console.log(json);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    var c = fetchData();
+    c.then(data => setPost(data.data.sort((a, b) => a.Title.localeCompare(b.Title))
+    ))
+  }, []);
+  
+  return <div>
+    <ul>
+      {post.map(item => <li key={item.Title}>{item.Title}</li>)}
+    </ul>
+  </div>;
+};
+
 
 export default App;
